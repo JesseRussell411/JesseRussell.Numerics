51b5cd2ad54964996db9dbdccde9c1895e126573
techClubMember <jesserussell411@gmail.com>
2020-12-06 13:41:27 -0700

    Replaced multiple fields with a single object field in IntFloat. Still a work in progress though.

---
+++ Debugging/Debugging.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using JesseRussell.Numerics;
+
+namespace Debugging
+{
+    class Debugging
+    {
+        static void Main(string[] args)
+        {
+            IntFloat bob = default;
+            Console.WriteLine(bob);
+            bob = 9;
+            Console.WriteLine(bob);
+            bob = 9.0;
+            Console.WriteLine(bob);
+            bob = 34e200;
+            Console.WriteLine(bob);
+            bob = (float)34e100;
+            Console.WriteLine(bob);
+        }
+    }
+}
